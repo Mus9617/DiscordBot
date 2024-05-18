8d9b6e7c936dcb3e8af48a00ec11cebbcb6b086a
Mus9617 <speede078@gmail.com>
2024-05-18 11:56:47 +0200

    main

---
+++ src/index.js
@@ -0,0 +1,56 @@
+require("dotenv").config();
+
+const {
+  Client,
+  GatewayIntentBits,
+  EmbedBuilder,
+  SlashCommandBuilder,
+  PermissionsBitField,
+  Permissions,
+} = require("discord.js");
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+client.on("ready", (x) => {
+  console.log(`${x.user.tag} is ready`);
+  client.user.setActivity("Desarrollado por: zowix_96");
+
+  const ping = new SlashCommandBuilder()
+    .setName("ping")
+    .setDescription("Replies with Pong!");
+
+    const buenas = new SlashCommandBuilder()
+    .setName("buenas")
+    .setDescription("Te saluda!");
+
+    const creador = new SlashCommandBuilder()
+    .setName("creador")
+    .setDescription("Pregunta Quien La Desarollo?");
+
+
+  
+  client.application.commands.create(ping);
+  client.application.commands.create(buenas);
+  client.application.commands.create(creador);
+});
+
+client.on("interactionCreate", async (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
+  if (interaction.commandName === "ping") {
+    interaction.reply("Pong!");
+  }
+  if (interaction.commandName === "buenas") {
+    interaction.reply("Buenas! Como estas?");
+  }
+  if (interaction.commandName === "creador") {
+    interaction.reply("Mi Desarrollador Es zowix_96 ");
+  }
+});
+
+client.login( process.env.TOKEN
+);
